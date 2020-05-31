fa603bf72564284c78e30583d7455fe2bbcc8aa5
Gopal Reddy Karri <gopalreddykarri@gmail.com>
2020-05-31 10:39:53 -0400

    updated gradle wrapper

--- src/main/java/com/gopal/springannotations/controller/JobController.java
+++ src/main/java/com/gopal/springannotations/controller/JobController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/job")
+@RequestMapping(path = "/v1/job")
 public class JobController {
 
     @GetMapping(path = "/details/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
